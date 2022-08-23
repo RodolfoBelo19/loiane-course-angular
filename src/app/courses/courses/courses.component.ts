0742fbce18fb9844f410749168aa92653a659653
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2022-08-23 18:15:31 -0300

    fix

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = this.coursesService.list();
+  displayedColumns = ['name', 'category'];
+
+
+  constructor(
+    private coursesService: CoursesService
+  ) {}
 
   ngOnInit(): void {
   }
 
 }
+
+// export class CoursesComponent implements OnInit {
+
+//   courses: Course[] = []
+//   displayedColumns = ['name', 'category'];
+
+//   courseService: CoursesService;
+
+//   constructor() {
+//     this.courseService = new CoursesService();
+//     this.courses = this.courseService.list();
+//   }
+
+//   ngOnInit(): void {
+//   }
+
+// }
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  list(): Course[] {
+    return [
+      { _id: "asdasdad", name: "Angular", category: "FrontEnd" },
+      { _id: "asdasdad", name: "Angular", category: "FrontEnd" },
+      { _id: "asdasdad", name: "Angular", category: "FrontEnd" },
+      { _id: "asdasdad", name: "Angular", category: "FrontEnd" },
+    ];
+  }
+}
