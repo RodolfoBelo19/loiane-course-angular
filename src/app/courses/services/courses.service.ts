8aac55da85f0f7a6c60b98ec84196c6316517fc7
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2022-08-23 22:59:25 -0300

    pipe icon

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -18,7 +18,6 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(1000),
       tap(courses => console.log(courses))
     )
   }
